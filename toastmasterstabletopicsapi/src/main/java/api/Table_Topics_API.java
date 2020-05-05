572935ec122b93bbef6875d50d421bb032eb720a
Akeem Adkins <adkinsakeem@gmail.com>
2020-05-05 16:55:39 -0400

    Fixed the RestController issue

--- toastmasterstabletopicsapi/src/main/java/api/Table_Topics_API.java
+++ toastmasterstabletopicsapi/src/main/java/api/Table_Topics_API.java
@@ -5,21 +5,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Table_Topics_API {
-	
-	
-	@RequestMapping(value = "/{p1}/{p2}/getsession", method = RequestMethod.GET,
-	        produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public String get(@PathVariable String p1, @PathVariable String p2) {
-		return "test";
-	}
-	
-	@RequestMapping(value = "/test", method = RequestMethod.GET,
+
+	@RequestMapping(value = "/test/", method = RequestMethod.GET,
 	        produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public String testFunction() {
+		System.out.println("Test2");
 		return "test";
 	}
 
